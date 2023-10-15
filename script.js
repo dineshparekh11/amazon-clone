a5b1f2ed6e1f325b4e82245d2ad85f60ea1c08ef
Dinesh Parekh <dinesh21045@gmail.com>
2023-10-16 00:47:08 +0530

    version2

---
+++ script.js
@@ -0,0 +1,27 @@
+const slides = document.querySelectorAll(".slide") 
+var counter = 0;
+
+// console.log(slides)
+
+slides.forEach(
+    (slide,index ) => {
+        slide.style.left = `${index * 100}%`
+})
+
+const goNext = () => {
+    counter++
+    slideImage();
+}
+
+const goPrev = () => {
+    counter--
+    slideImage();
+}
+
+const slideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter * 100}%)`
+        }
+    )
+}
